3f46470054fdee0645f7332d4479d4d6d683c3a1
Patrik Kadlecek <kadlenko40@gmail.com>
2026-03-31 22:35:43 +0200

    DONE:    - added basic tests for add,subtract,multiply,divide
    TODO:
       - tests for factorial, sum, power, root
       - add floating point numbers to tests
    
    FIX:
       - Makefile not connecting to gtests correctly

--- src/math_lib_tests.cpp
+++ src/math_lib_tests.cpp
@@ -1,5 +1,73 @@
+#include <gtest/gtest.h>
 #include "math_lib.h"
 
-int main() {
-    return 0;
+TEST(Add, BasicCases) {
+    EXPECT_DOUBLE_EQ(add(5,5),10);
+    EXPECT_DOUBLE_EQ(add(-5,-5),-10);
+    EXPECT_DOUBLE_EQ(add(5,-5),0);
+    EXPECT_DOUBLE_EQ(add(5,0),5);
+    EXPECT_DOUBLE_EQ(add(0,5),5);
+    EXPECT_DOUBLE_EQ(add(0,0),0);
+}
+
+TEST(Subtract, BasicCases) {
+    EXPECT_DOUBLE_EQ(subtract(5,5),0);
+    EXPECT_DOUBLE_EQ(subtract(-5,-5),0);
+    EXPECT_DOUBLE_EQ(subtract(-5,5),-10);
+    EXPECT_DOUBLE_EQ(subtract(5,-5),10);
+    EXPECT_DOUBLE_EQ(subtract(5,0),5);
+    EXPECT_DOUBLE_EQ(subtract(0,0),0);
+}
+
+TEST(Multiply, BasicCases) {
+    EXPECT_DOUBLE_EQ(multiply(5,5),25); 
+    EXPECT_DOUBLE_EQ(multiply(-5,-5),25);
+    EXPECT_DOUBLE_EQ(multiply(5,-5),-25);
+    EXPECT_DOUBLE_EQ(multiply(0,5),0);
+    EXPECT_DOUBLE_EQ(multiply(0,0),0);
+}
+
+TEST(Divide, BasicCases) {
+    EXPECT_DOUBLE_EQ(divide(25,5), 5);
+    EXPECT_DOUBLE_EQ(divide(-25,-5), 5);
+    EXPECT_DOUBLE_EQ(divide(25,-5), -5);
+    EXPECT_DOUBLE_EQ(divide(5,5), 1);
+    EXPECT_DOUBLE_EQ(divide(5,1), 5);
+
+}
+
+TEST(Divide, DivisionByZero) {
+    EXPECT_DOUBLE_EQ(divide(0,5),0);
+
+    EXPECT_THROW(divide(5,0), std::invalid_argument);
+    EXPECT_THROW(divide(0,0), std::invalid_argument);
+
+}
+
+/*
+TODO
+TEST(Factorial,) {
+
+}
+
+
+TEST(Sum,) {
+
+}
+
+
+TEST(Power) {
+
+}
+
+
+TEST(Root) {
+
+}
+
+*/
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
